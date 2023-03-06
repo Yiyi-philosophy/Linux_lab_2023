f2339a97d9fe213f861f7d04022e6fd710850ccf
“Yiyi-philosophy” <yiyidematthew6@outlook.com>
2023-03-06 20:44:06 +0800

    deleted:    lab1/test.c 	modified:   lab1/test.sh

--- lab1/test.sh
+++ lab1/test.sh
@@ -1,31 +1,54 @@
 #!/bin/bash
-echo "Begin testing ...\n"
+echo "Begin testing ..."
 
-list_file = `ls ./src`
-for file in list_file
-do
-    cat a.txt > contain
-    if [-e a.txt] && [contain = 'Welcome'] 
-    then
-        echo "File a.txt is correct!\n"
-        if [-e b] && [contain = 'to'] 
-        then
-            echo "File b is correct!\n"
-            if [-e c.c] && [contain = 'Linux 2023'] 
-            then
-                echo "File c.c is correct!\n"
-                echo "All TEST PASS!\n"
-            else
-                echo "File c.c is wrong!"
-            fi
-        else
-            echo "File b is wrong!"
-        fi
-    else
-        echo "File a.txt is wrong!"
-    fi
+list=`ls ./src`
+
+
+if [ $list !="a.txt b c.c" ]
+then
+    echo "Number of files is wrong "
+else
+    echo "Number of files is correct "
     
-done
+    for file in $list
+    do
+        cat ./src/$file > contain
+        if [ -f "./src/a.txt" ] && [ "$contain"="Welcome" ] 
+        then
+            echo "File a.txt is correct!"
+            break
+        else
+            echo "File a.txt is wrong!"
+            break
+        fi
+    done
 
+    # for file in $list
+    # do
+    #     $contain <./src/$file
+    #     if [ -f "b" ] && [ "$contain"='to' ] 
+    #     then
+    #         echo "File b is correct!"
+    #         break
+    #     else
+    #         echo "File b is wrong!"
+    #         break   
+    #     fi
+    # done
 
-echo "Test finished\n"
+    # for file in $list
+    # do
+    #     $contain < ./src/$file
+    #     if [ -f "c.c" ] && [ "$contain"="Linux 2023" ] 
+    #     then
+    #         echo "File c.c is correct!"
+    #         echo "All TEST PASS!!!"
+    #         break
+    #     else
+    #         echo "File c.c is wrong!"
+    #         break
+    #     fi
+    # done
+fi
+
+echo "Test finished"
