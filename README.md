964e92cee62eae05717a57195d5280b76ebf4bc9
“Yiyi-philosophy” <yiyidematthew6@outlook.com>
2023-03-16 09:47:34 +0800

    modified:   README.md 	new file:   image/1678930749732.png 	deleted:    lab1/src_2/a.txt 	deleted:    lab1/src_2/b 	deleted:    lab1/src_2/c.c 	modified:   lab1/test.sh

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
 # Linux 2023 Lab
 
-Hello! This is our lab. Please download the code and run in linux system.
+Hello! This is our lab. Please download the code by 
+
+`git clone https://github.com/Yiyi-philosophy/Linux_lab_2023`
+
+and run in linux system.
 
 ## Lab 1
 
 Copy `a.txt` `b` `c.c` files from `org` to `src .`
 
+We hope that file in `src` is like this:
+
+- `a.txt`
+  - Content: "Welcome"
+- `b`
+  - Content: "to"
+- `c`
+  - Content: "Linux 2023"
+
 You can use `./test.sh` to vertify your answer.
+
+
+![1678930749732](image/1678930749732.png)
--- lab1/test.sh
+++ lab1/test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 echo "Begin testing ..."
-
-list=`ls ./src` # a b c should pass
+path='./src'
+list=`ls $path` 
+# a b c should pass
 # list=`ls ./src_2` b has problem
 # list=`ls ./src_2` c.c has problem
+
 list_ref=`echo -e 'a.txt\nb\nc.c\n'`
 flag1=0
 flag2=0
@@ -17,44 +19,38 @@ else
     
     for file in $list
     do
-        if "${file##*/}" = "a.txt"
+        # if [ "${file##*/}" = "a.txt" ]
+        if [ "$file" = "a.txt" ]
         then
-            contain1=`cat ./src/$file`
-            echo $contain1
+            contain1=`cat $path/$file`
             if [ "$contain1" = "Welcome" ]
             then
                 echo "File a.txt is correct!"
                 flag1=1
-                break
             else
                 echo "File a.txt is wrong!"
-                break 
             fi  
 
-        elif "$file" = "b"
+        elif [ "${file##*/}" = "b" ]
         then
-            contain2=`cat ./src/$file`
+            contain2=`cat $path/$file`
             if [ "$contain2" = 'to' ]
             then
                 echo "File b is correct!"
                 flag2=1
-                break
             else
                 echo "File b is wrong!"
-                break 
             fi 
 
-        elif [ "$file" = "c.c" ]
+        elif [ "${file##*/}" = "c.c" ]
         then
-            contain3=`cat ./src/$file`
-            if [ "$contain3" = 'Linux 2023']
+            contain3=`cat $path/$file`
+            if [ "$contain3" = "Linux 2023" ]
             then
                 echo "File c.c is correct!"
                 flag3=1
-                break
             else
                 echo "File c.c is wrong!"
-                break
             fi
 
         else
@@ -68,5 +64,5 @@ if [ $flag1 -eq 1 ] && [ $flag2 -eq 1 ] && [ $flag3 -eq 1 ]
 then 
     echo "All TEST PASS!!!"
 else
-    echo "Something wrong"
+    echo "Something Wrong"
 fi
